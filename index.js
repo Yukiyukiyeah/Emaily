4cb2334aa92f285cfc01e5b381a945e54bd77d7e
Yuki <yukitang@mail.bnu.edu.cn>
2020-12-20 13:50:23 +0800

    add Google oauth

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 /*
  * @Author: Yuki
  * @Date: 2020-12-18 21:16:31
- * @LastEditTime: 2020-12-18 21:35:35
+ * @LastEditTime: 2020-12-20 13:48:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /server/index.js
  */
+
 const express = require('express');
+const passport = require('passport');
+const  GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+  }, accessToken => {
+    console.log(accessToken);
+  })
+);
 
+app.get(
+  'auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+// Dynamically tells us which port to listen to (Heroku injected to Environmental variable)
 const PORT = process.env.PORT || 5000 // environment variable
 app.listen(PORT);
