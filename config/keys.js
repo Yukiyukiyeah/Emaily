6adb41fef60b0b1a369e524ebe2bc411929c02bc
Yuki <yukitang@mail.bnu.edu.cn>
2020-12-22 02:56:24 +0800

    add a ;

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-20 13:19:06
- * @LastEditTime: 2020-12-20 23:08:46
+ * @LastEditTime: 2020-12-22 02:55:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /server/config/keys.js
@@ -9,7 +9,7 @@
 // keys.js -figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
-  module.exports = require('./prod')
+  module.exports = require('./prod');
 } else {
   // we are in development -return the dev keys!
   module.exports = require('./dev');
