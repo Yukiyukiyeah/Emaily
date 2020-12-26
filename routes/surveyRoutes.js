9d77108be59998e55334d7ac7c454bbf97421b6b
Yuki <yukitang@mail.bnu.edu.cn>
2020-12-26 22:13:47 +0800

    path-parser

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-23 11:29:29
- * @LastEditTime: 2020-12-26 21:39:50
+ * @LastEditTime: 2020-12-26 22:13:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /server/routes/surveyRoutes.js
@@ -11,6 +11,7 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const { Path } = require('path-parser');
 
 const Survey = mongoose.model('surveys');
 
